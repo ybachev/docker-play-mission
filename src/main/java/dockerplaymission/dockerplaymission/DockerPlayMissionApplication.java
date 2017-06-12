30058b5b2d75e76c5d74298fef0a514fe2c75e69
Yavor Bachev <yavor.bachev@gmail.com>
2017-06-12 15:32:31 +0300

    init - docker-play-mission;

---
+++ src/main/java/dockerplaymission/dockerplaymission/DockerPlayMissionApplication.java
@@ -0,0 +1,12 @@
+package dockerplaymission.dockerplaymission;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerPlayMissionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerPlayMissionApplication.class, args);
+	}
+}
---
+++ src/test/java/dockerplaymission/dockerplaymission/DockerPlayMissionApplicationTests.java
@@ -0,0 +1,16 @@
+package dockerplaymission.dockerplaymission;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DockerPlayMissionApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
